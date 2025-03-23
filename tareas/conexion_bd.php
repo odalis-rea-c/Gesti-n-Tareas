fef931044dfe09cd91be0244c6e802c674caf45d
odalis-rea-c <osxyrea@gmail.com>
2025-03-23 00:28:04 -0500

    Add files via upload

---
+++ tareas/conexion_bd.php
@@ -0,0 +1,13 @@
+<?php
+
+$conexion= new mysqli('localhost', 'root', '', 'gestiontareas');
+
+/* 
+ if ($conexion) {
+   echo 'Conexion Exitosa';
+} else {
+    echo 'Conexion No Exitosa';
+} */
+
+
+?>
